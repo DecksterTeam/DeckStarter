457dc9192b863f59bc7f67a036c72b213cfca42b
Steven Heinrich <steven.heinrich@gmail.com>
2015-02-04 12:29:04 -0500

    Fix HeaderView to follow backbone style

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,16 +7,21 @@
             'text': '../../lib/vendor/require-text/text',
             'jquery': '../../lib/vendor/jquery-1.11.2.min',
             'bootstrap': '../../lib/vendor/bootstrap-3.1.1-dist/js/bootstrap.min',
-            'handlebars': '../../lib/vendor/handlebars/handlebars'
+            'handlebars': '../../lib/vendor/handlebars/handlebars',
+            'backbone': '../../lib/vendor/backbone/backbone',
+            'underscore': '../../lib/vendor/underscore/underscore'
         },
         shim: {
             'bootstrap' : {
                 deps : ['jquery']
+            },
+            'backbone' : {
+                deps : ['jquery', 'underscore']
             }
         }
     });
 
-    require( ['app', 'jquery', 'bootstrap'], 
+    require( ['app', 'jquery', 'underscore', 'bootstrap', 'backbone'], 
         function(App, $) {
             App.initialize();
     });
