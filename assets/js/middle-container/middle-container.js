dce7e256b7d7532c7d0ffaea640bd16967d9598d
cherbert <cherbert@42six.com>
2015-02-04 20:31:53 -0500

    Added Gridster Basic Functionality

--- assets/js/middle-container/middle-container.js
+++ assets/js/middle-container/middle-container.js
@@ -2,9 +2,9 @@ define([
     'jquery',
     'text!middle-container/middle-container.hbs',
     'handlebars',
-    'bootstrap'
+    'bootstrap',
+	'gridster'
 ], function ($, MiddleContainerHBS, Handlebars) {
-
     'use strict';
 
     return {
@@ -12,8 +12,25 @@ define([
             var middleContainerViewTemplate = Handlebars.compile(MiddleContainerHBS);
             var middleContainerViewHTML = middleContainerViewTemplate();
             this.$el = $(middleContainerViewHTML);
-            
+			this.initGrid();
             return this;
-        }
+        },
+		
+		initGrid: function(){
+			var gridster = $(this.$el).find('.gridster ul').gridster({
+	          widget_base_dimensions: [100, 100],
+	          widget_margins: [5, 5],
+	          helper: 'clone'
+	        }).data('gridster');
+			
+  // resize widgets on hover
+          gridster.$el
+            .on('mouseenter', '> li', function() {
+                gridster.resize_widget($(this), 3, 3);
+            })
+            .on('mouseleave', '> li', function() {
+                gridster.resize_widget($(this), 1, 1);
+            });
+		}
     };
 });
