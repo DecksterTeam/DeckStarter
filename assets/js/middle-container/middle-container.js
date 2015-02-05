053f431fa0018ad3e275964426ad7e918e63cbb7
cherbert <cherbert@42six.com>
2015-02-05 13:26:13 -0500

    Styling grid

--- assets/js/middle-container/middle-container.js
+++ assets/js/middle-container/middle-container.js
@@ -2,9 +2,8 @@ define([
     'jquery',
     'text!middle-container/middle-container.hbs',
     'handlebars',
-    'bootstrap',
-	'gridster'
-], function ($, MiddleContainerHBS, Handlebars) {
+    'text!middle-container/tile.hbs'
+], function ($, MiddleContainerHBS, Handlebars, TileHBS) {
     'use strict';
 
     return {
@@ -16,12 +15,12 @@ define([
         },
 		
 		initGrid: function(){
-			var gridElem = $(this.$el).find('.gridster ul');
+			this._gridParent = $(this.$el).find('.gridster ul');
 			var NUM_COLUMNS = this.gatherColumns();
 			var PADDING = 5;
 			
 			this._width = $('.gridster').innerWidth() - NUM_COLUMNS * PADDING * 2;
-			var gridster = gridElem.gridster({
+			var gridster = this._gridParent.gridster({
 	          widget_base_dimensions: [this._width / 4, 100],
 	          widget_margins: [5, 5],
 	          helper: 'clone',
@@ -31,19 +30,19 @@ define([
 	          }
 	        }).data('gridster');
 			
-  		  // // resize widgets on hover
-//           gridster.$el
-//             .on('mouseenter', '> li', function() {
-//                 gridster.resize_widget($(this), 3, 3);
-//             })
-//             .on('mouseleave', '> li', function() {
-//                 gridster.resize_widget($(this), 1, 1);
-//             });
+			this.renderTiles();
 		},
 		
 		gatherColumns: function(){
 			var col = $.map($(this.$el).find('.gridster ul li[data-col]'), function(e,i){return parseInt($(e).data('col'));});
 			return Math.max.apply(Math, col);
+		},
+		
+		renderTiles: function(){
+			this._gridParent.children().each(function(i, el){
+				var tile = Handlebars.compile(TileHBS);
+				$(el).html(tile());
+			});
 		}
     };
 });
