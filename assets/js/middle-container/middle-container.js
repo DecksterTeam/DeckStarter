d758d6ae4b83910094c67bfa8d1bc5389d1df207
cherbert <cherbert@42six.com>
2015-02-06 01:24:25 -0500

    Added tile templates, still need to do cleanup

--- assets/js/middle-container/middle-container.js
+++ assets/js/middle-container/middle-container.js
@@ -9,6 +9,7 @@ define([
 	var _gridParent;
 	var NUM_COLUMNS;
 	var PADDING;
+	var ROW_HEIGHT = 150;
 
 	var getGridElement = function(event){
 		return $(event.target).parents('li');
@@ -31,30 +32,37 @@ define([
 			
 			this._width = $('.gridster').innerWidth() - NUM_COLUMNS * PADDING * 2;
 			var gridster = _gridParent.gridster({
-	          widget_base_dimensions: [this._width / NUM_COLUMNS, 100],
-	          widget_margins: [5, 5],
-	          helper: 'clone',
-	          autogrow_cols: true,
-	          resize: {
-	            enabled: true
-	          }
+	          widget_base_dimensions: [this._width / NUM_COLUMNS, ROW_HEIGHT],
+	          widget_margins: [5, 5]
 	        }).data('gridster');
 			
 			this.renderTiles();
 		},
 		
 		gatherColumns: function(){
-			var col = $.map($(this.$el).find('.gridster ul li[data-col]'), function(e,i){return parseInt($(e).data('col'));});
+			var col = $.map($(this.$el).find('.gridster ul li[data-col]'),
+			 function(e,i){return parseInt($(e).data('col'));});
 			return Math.max.apply(Math, col);
 		},
 		
 		renderTiles: function(){
+			var curr = this;
 			_gridParent.children().each(function(i, el){
 				var tile = Handlebars.compile(TileHBS);
+				curr.applyTileTemplate(el);
 				$(el).html(tile());
 			});
 			$('[data-tile-fullscreen]').on('click', this.resizeTile);
 			$('[data-tile-visible]').on('click', this.closeTile);
+		},
+		
+		applyTileTemplate: function(el){
+			var t = $(el).data('template');
+			if(t){
+				require(['text!tiles/' + t + '.hbs'], function(innerTile){
+					$(el).find('.parent').append(innerTile);
+				});
+			}
 		},
 		
 		resizeTile: function(event){
@@ -62,11 +70,13 @@ define([
 			if(event.target.dataset.tileFullscreen === "true"){
 				$(elem).data('o_x', $(elem).data('sizex'));
 				$(elem).data('o_y', $(elem).data('sizey'));
-				_gridParent.gridster().data('gridster').resize_widget(elem, NUM_COLUMNS, 4);
+				_gridParent.gridster().data('gridster').resize_widget(elem, 
+					NUM_COLUMNS - parseInt($(elem).data('col')) + 1, 4);
 				event.target.dataset.tileFullscreen = "false";
 			}
 			else {
-				_gridParent.gridster().data('gridster').resize_widget(elem, $(elem).data('o_x'), $(elem).data('o_y'));
+				_gridParent.gridster().data('gridster').resize_widget(elem, 
+					$(elem).data('o_x'), $(elem).data('o_y'));
 				event.target.dataset.tileFullscreen = "true"
 			}
 		},
