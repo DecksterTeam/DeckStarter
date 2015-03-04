8bd45930510938e5d647a7f3697c183ad0b520ab
cherbert <cherbert@42six.com>
2015-03-03 20:36:42 -0500

    Added deckster libraries

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,8 +8,11 @@
             'jquery': '../../lib/vendor/jquery-1.11.2.min',
             'bootstrap': '../../lib/vendor/bootstrap-3.1.1-dist/js/bootstrap.min',
             'handlebars': '../../lib/vendor/handlebars/handlebars',
-			'gridster': '../../lib/vendor/gridster/jquery.gridster.min',
-			'radio': '../../lib/vendor/radio/radio.min'
+			'gridster': '../../lib/vendor/gridster/jquery.gridster',
+			'radio': '../../lib/vendor/radio/radio.min',
+			'angular': '../../lib/vendor/angular/angular.min.js',
+			'angular-route': '../../lib/vendor/angular-route/angular-route.min.js',
+			'deckster': '../../lib/vendor/decksterjs/dist/jquery.deckster.min'
         },
         shim: {
             'bootstrap' : {
